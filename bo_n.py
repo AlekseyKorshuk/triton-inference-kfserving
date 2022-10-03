095ee08329b3fc4134c747e24269df82ec3da401
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-04 00:15:15 +0300

    updates

--- bo_n.py
+++ bo_n.py
@@ -230,7 +230,9 @@ class KFServingHuggingFace(kfserving.KFModel):
                 value_data = [[len(sample_input_ids)] for sample_input_ids in input_ids]
                 data = np.array([data for data in value_data], dtype=value['dtype'])
             elif value['name'] == 'random_seed':
-                data = np.array([[random.randint(0, 10000000)] for _ in range(len(input_ids))], dtype=value['dtype'])
+                random_seed = 0
+                # random_seed = random.randint(0, 10000000)
+                data = np.array([[random_seed] for _ in range(len(input_ids))], dtype=value['dtype'])
             else:
                 data = np.array([data for data in value['data']] * len(input_ids), dtype=value['dtype'])
 
